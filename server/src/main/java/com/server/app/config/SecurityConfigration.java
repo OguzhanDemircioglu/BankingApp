a78fd919665405045c329e65d4bed0d85b980b68
oguz <oguzhanturgut611@gmail.com>
2024-02-28 22:36:59 +0300

    swaggerFix

--- server/src/main/java/com/server/app/config/SecurityConfigration.java
+++ server/src/main/java/com/server/app/config/SecurityConfigration.java
@@ -4,9 +4,11 @@ import com.server.app.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -25,16 +27,20 @@ public class SecurityConfigration {
     private final UserService userService;
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-        httpSecurity.csrf(AbstractHttpConfigurer::disable)
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(req -> req.requestMatchers("/api/auth/**").permitAll()
-                        .requestMatchers("api/**").hasAnyAuthority("ROLE_ADMIN")
-                        .anyRequest().authenticated())
+                        .requestMatchers("/h2-console/**").permitAll()
+                        .requestMatchers("/api/**").hasAnyAuthority("ROLE_ADMIN")
+                        .anyRequest().fullyAuthenticated())
+                .httpBasic(Customizer.withDefaults())
                 .sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider()).addFilterBefore(
                         jwtAuthanticationFilter, UsernamePasswordAuthenticationFilter.class);
-        return httpSecurity.build();
+
+        return http.build();
     }
+
 
     @Bean
     public AuthenticationProvider authenticationProvider(){
